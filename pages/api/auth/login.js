9356a4d304847c430890b0fc42694f11ee6a0267
Emiro Trechy <e.trechy.p@gmail.com>
2023-06-14 16:25:34 -0400

    bug fixed

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -14,7 +14,6 @@ export default async function loginHandler(req, res) {
     if (!email || !password) {
         return res.status(400).json({ message: 'Please provide an email and password' })
     } else {
-        //const user = await User.findOne({ email }).select('+password')
         const user = await User.findOne({ email })
         if (user) {
             let checkPass = await bcrypt.compare(password, user.password)
@@ -23,20 +22,12 @@ export default async function loginHandler(req, res) {
                     email: user.email,
                     name: user.name
                 }, secret, { expiresIn: '1h' })
-                /* const token = JWT.sign({
-                    exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 7),
-                    email: user.email,
-                    name: user.name
-                }, process.env.SECRET,
-                    {
-                        algorithm: 'HS384'
-                    }) */
-
+                
                 const serializedToken = serialize('token', token, {
                     httpOnly: true,
                     secure: process.env.NODE_ENV !== 'development',
                     sameSite: 'strict',
-                    maxAge: 60 * 60 * 24 * 7,
+                    maxAge: 3600,
                     path: '/'
                 })
 
